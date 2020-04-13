12a15bfcbb1dc0e77caaa1a3ef71ac69f1422fa3
zhaoxukl1314 <zhaoxuk13kl>
2020-04-13 21:53:58 +0800

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,68 @@
 import { Component } from '@angular/core';
 
+interface TopMenu {
+  title: string;
+  link: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'pinduoduo';
+  title = '呵呵哒';
+  menus: TopMenu[] = [{
+    title: '热门',
+    link: ''
+  }, {
+    title: '男装',
+    link: ''
+  }, {
+    title: '百货',
+    link: ''
+  }, {
+    title: '运动',
+    link: ''
+  }, {
+    title: '手机',
+    link: ''
+  }, {
+    title: '家纺',
+    link: ''
+  }, {
+    title: '视频',
+    link: ''
+  }, {
+    title: '电器',
+    link: ''
+  }, {
+    title: '鞋包',
+    link: ''
+  }, {
+    title: '汽车',
+    link: ''
+  }, {
+    title: '水果',
+    link: ''
+  }, {
+    title: '电脑',
+    link: ''
+  }, {
+    title: '内衣',
+    link: ''
+  }, {
+    title: '家装',
+    link: ''
+  }, {
+    title: '母婴',
+    link: ''
+  }, {
+    title: '美妆',
+    link: ''
+  }, {
+    title: '家具',
+    link: ''
+  }];
 }
